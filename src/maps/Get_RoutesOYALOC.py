0eaf9cadff87c003b9fa8048c91ce602ccf12a50
Daniel Souza <82693381+DSouzaR67@users.noreply.github.com>
2024-07-07 16:35:24 -0600

    Add files via upload

---
+++ src/maps/Get_RoutesOYALOC.py
@@ -0,0 +1,7 @@
+import googlemaps
+
+# Tu clave de API de Google Maps
+api_key = 'AIzaSyAZBPYol89oaQXB2RwIHpIanDSpEH0NXNo'
+
+# Inicializa el cliente de Google Maps
+gmaps = googlemaps.Client(key=api_key)
